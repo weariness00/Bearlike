4861f95fd9f726595109cdb8d0a8c207b92f3a36
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-01-28 20:16:57 +0900

    싱글톤 패턴 Util 추가

---
+++ Bearlike Project/Assets/Scripts/Util/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Util
+{
+    public class Singleton<T> : MonoBehaviour, ISingleton where T : Component, new()
+    {
+        public static T Instance
+        {
+            get
+            {
+                Init();
+                return _instance;
+            }
+        }
+        private static T _instance = null;
+
+        protected virtual void Awake()
+        {
+            if (_instance != null &&
+                _instance.gameObject != gameObject)
+            {
+                DestroyImmediate(gameObject);
+            }
+        }
+
+        private static void Init()
+        {
+            if (_instance == null)
+            {
+                var singletonObject = new GameObject(typeof(T).Name);
+                _instance = singletonObject.AddComponent<T>();
+            }
+        }
+    }
+}
