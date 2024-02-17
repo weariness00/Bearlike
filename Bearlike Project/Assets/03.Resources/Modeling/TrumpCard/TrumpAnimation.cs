00df289ea8cfae9f2631cf8ad35f39062b59b977
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-02-18 00:24:30 +0900

    모델링 추가
    기타 수정

---
+++ Bearlike Project/Assets/03.Resources/Modeling/TrumpCard/TrumpAnimation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrumpAnimation : MonoBehaviour
+{
+    public enum WalkAnimation
+    {
+        Idle,
+        Walk
+    }
+}
