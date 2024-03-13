2ea4b5b50432effcf8cf2afb6022a98ab7399cad
Seo Dong Woo <ehddn917@gmail.com>
2024-03-14 00:58:36 +0900

    보스 스테이지 추가

---
+++ Bearlike Project/Assets/01.Scripts/GamePlay/Stage/Container/StageBoss.cs
@@ -0,0 +1,31 @@
+﻿using GamePlay.StageLevel;
+using Monster;
+using Script.Photon;
+
+namespace GamePlay.Stage.Container
+{
+    public class StageBoss : StageLevelBase
+    {
+        public NetworkSpawner bossSpawner;
+
+        public override void StageStart()
+        {
+            base.StageStart();
+            bossSpawner.SpawnSuccessAction += (obj) =>
+            {
+                var monster = obj.GetComponent<MonsterBase>();
+                monster.DieAction += () =>
+                {
+                    isStageClear = true;
+                };
+            };
+            bossSpawner.SpawnStart();
+        }
+
+        public override void StageClear()
+        {
+            base.StageClear();
+            
+        }
+    }
+}
