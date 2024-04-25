48f651ecb01df2af09fa9876ccef29dca5f80b34
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-04-25 18:21:31 +0900

    Mesh Slice를 Compute Shader로 작성중
    Mesh Slice 과정을 Compute Shader에서 하도록 수정 완료
    
    Compute Shader가 내준 결과값을 토대로 새로운 Mesh 생성하기

--- Bearlike Project/Assets/00.Scenes/Test ( Dong Woo )/Mesh Destruction/Test1.cs
+++ Bearlike Project/Assets/00.Scenes/Test ( Dong Woo )/Mesh Destruction/Test1.cs
@@ -15,12 +15,23 @@ namespace Test
             public const string MeshDataConvertKernel = "MeshDataConvert";
             public const int ThreadX = 32;
             
+            public static readonly int SlicePoint = Shader.PropertyToID("slicePoint");
+            public static readonly int SliceNormal = Shader.PropertyToID("sliceNormal");
+            
             public static readonly int Vertices = Shader.PropertyToID("vertices");
             public static readonly int Normals = Shader.PropertyToID("normals");
             public static readonly int UVs = Shader.PropertyToID("uvs");
             public static readonly int Triangles = Shader.PropertyToID("triangles");
-            public static readonly int TriangleLength = Shader.PropertyToID("triangleLength");
-            public static readonly int OutputPolygonData = Shader.PropertyToID("outputPolygonData");
+            public static readonly int PolygonLength = Shader.PropertyToID("polygonLength");
+            public static readonly int DotLength = Shader.PropertyToID("dotLength");
+            
+            public static readonly int SliceData0 = Shader.PropertyToID("sliceData0");
+            public static readonly int SliceData1 = Shader.PropertyToID("sliceData1");
+            public static readonly int SliceCount0 = Shader.PropertyToID("sliceCount0");
+            public static readonly int SliceCount1 = Shader.PropertyToID("sliceCount1");
+
+            public static readonly int NewDotData = Shader.PropertyToID("newDotData");
+            public static readonly int NewDotCount = Shader.PropertyToID("newDotCount");
         }
         
         #region Unity Enven Function
@@ -33,33 +44,6 @@ namespace Test
         #endregion
         
         #region Data Structure
-        /// <summary>
-        /// 쪼개진 mesh 정보들을 담을 클래스
-        /// </summary>
-        public class SliceInfo
-        {
-            public List<MeshDotData> DotList = new List<MeshDotData>();
-            public List<int> Triangles = new List<int>();
-
-            public SliceInfo(params SliceInfo[] infos)
-            {
-                foreach (var sliceInfo in infos)
-                {
-                    this.AddRange(sliceInfo);
-                }
-            }
-
-            public void AddRange(SliceInfo other)
-            {
-                // 폴리곤의 버텍스 인덱스 재정의
-                for (int i = 0; i < other.Triangles.Count; i++)
-                {
-                    other.Triangles[i] += DotList.Count;
-                }
-                DotList.AddRange(other.DotList);
-                Triangles.AddRange(other.Triangles);
-            }
-        }
 
         /// <summary>
         /// 폴리곤 데이터를 담을 구조체
@@ -94,6 +78,8 @@ namespace Test
         public void Slice(GameObject targetObject, Vector3 sliceNormal, Vector3 slicePoint)
         {
             Mesh mesh = targetObject.GetComponent<MeshFilter>().sharedMesh;
+            slicePoint = targetObject.transform.position - slicePoint;
+            
             int dotCount = mesh.vertices.Length;
             int triangleCount = mesh.triangles.Length;
             int polygonCount = triangleCount / 3;
@@ -101,31 +87,75 @@ namespace Test
             ComputeBuffer normalsBuffer = new ComputeBuffer(dotCount, sizeof(float) * 3);
             ComputeBuffer uvsBuffer = new ComputeBuffer(dotCount, sizeof(float) * 2);
             ComputeBuffer trianglesBuffer = new ComputeBuffer(triangleCount, sizeof(int));
-            ComputeBuffer polygonsBuffer = new ComputeBuffer(polygonCount, Marshal.SizeOf(typeof(PolygonData)));
-            PolygonData[] polygonData = new PolygonData[polygonCount];
+            ComputeBuffer dotLengthBuffer = new ComputeBuffer(1, sizeof(uint));
+            
+            ComputeBuffer sliceDataBuffer0 = new ComputeBuffer(polygonCount, Marshal.SizeOf(typeof(PolygonData)));
+            ComputeBuffer sliceDataBuffer1 = new ComputeBuffer(polygonCount, Marshal.SizeOf(typeof(PolygonData)));
+            ComputeBuffer newDotDataBuffer = new ComputeBuffer(polygonCount * 2, Marshal.SizeOf(typeof(MeshDotData)));
+            ComputeBuffer sliceCountBuffer0 = new ComputeBuffer(1, sizeof(uint));
+            ComputeBuffer sliceCountBuffer1 = new ComputeBuffer(1, sizeof(uint));
+            ComputeBuffer newDotCountBuffer = new ComputeBuffer(1, sizeof(uint));
+                
+            PolygonData[] slicePolygonData0 = new PolygonData[polygonCount];
+            PolygonData[] slicePolygonData1 = new PolygonData[polygonCount];
+            MeshDotData[] newDotData = new MeshDotData[polygonCount * 2];
+            uint[] sliceCount0 = new uint[1];
+            uint[] sliceCount1 = new uint[1];
+            uint[] newDotCount = new uint[1];
+            uint[] dotLength = new uint[]{(uint)dotCount};
             
             verticesBuffer.SetData(mesh.vertices);
             normalsBuffer.SetData(mesh.normals);
             uvsBuffer.SetData(mesh.uv);
             trianglesBuffer.SetData(mesh.triangles);
+            dotLengthBuffer.SetData(dotLength);
+            
+            sliceDataBuffer0.SetData(slicePolygonData0);
+            sliceDataBuffer1.SetData(slicePolygonData1);
+            newDotDataBuffer.SetData(newDotData);
+            sliceCountBuffer0.SetData(sliceCount0);
+            sliceCountBuffer1.SetData(sliceCount1);
+            newDotCountBuffer.SetData(newDotCount);
             
             int kernelID = polygonShader.FindKernel(CSParam.MeshDataConvertKernel);
-            polygonShader.SetInt(CSParam.TriangleLength, polygonCount);
+            polygonShader.SetInt(CSParam.PolygonLength, polygonCount);
+            polygonShader.SetVector(CSParam.SlicePoint, slicePoint);
+            polygonShader.SetVector(CSParam.SliceNormal, sliceNormal);
             polygonShader.SetBuffer(kernelID, CSParam.Vertices, verticesBuffer);
             polygonShader.SetBuffer(kernelID, CSParam.Normals, normalsBuffer);
             polygonShader.SetBuffer(kernelID, CSParam.UVs, uvsBuffer);
             polygonShader.SetBuffer(kernelID, CSParam.Triangles, trianglesBuffer);
-            polygonShader.SetBuffer(kernelID, CSParam.OutputPolygonData, polygonsBuffer);
+            polygonShader.SetBuffer(kernelID, CSParam.DotLength, dotLengthBuffer);
+            
+            polygonShader.SetBuffer(kernelID, CSParam.SliceData0, sliceDataBuffer0);
+            polygonShader.SetBuffer(kernelID, CSParam.SliceData1, sliceDataBuffer1);
+            polygonShader.SetBuffer(kernelID, CSParam.NewDotData, newDotDataBuffer);
+            polygonShader.SetBuffer(kernelID, CSParam.SliceCount0, sliceCountBuffer0);
+            polygonShader.SetBuffer(kernelID, CSParam.SliceCount1, sliceCountBuffer1);
+            polygonShader.SetBuffer(kernelID, CSParam.NewDotCount, newDotCountBuffer);
 
             var threadX = polygonCount / CSParam.ThreadX;
             polygonShader.Dispatch(kernelID, threadX + 1,1,1);
             
-            polygonsBuffer.GetData(polygonData);
+            sliceDataBuffer0.GetData(slicePolygonData0);
+            sliceDataBuffer1.GetData(slicePolygonData1);
+            newDotDataBuffer.GetData(newDotData);
+            sliceCountBuffer0.GetData(sliceCount0);
+            sliceCountBuffer1.GetData(sliceCount1);
+            newDotCountBuffer.GetData(newDotCount);
             
             verticesBuffer.Release();
             normalsBuffer.Release();
             uvsBuffer.Release();
-            polygonsBuffer.Release();
+            trianglesBuffer.Release();
+            dotLengthBuffer.Release();
+            
+            sliceDataBuffer0.Release();
+            sliceDataBuffer1.Release();
+            newDotDataBuffer.Release();
+            sliceCountBuffer0.Release();
+            sliceCountBuffer1.Release();
+            newDotCountBuffer.Release();
         }
     }
 }
