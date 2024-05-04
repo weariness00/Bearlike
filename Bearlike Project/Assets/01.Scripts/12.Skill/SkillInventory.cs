602767405e2515b36a8dcea5652ec4640b8830e4
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-05-04 13:39:48 +0900

    스킬 인벤토리 수정

--- Bearlike Project/Assets/01.Scripts/12.Skill/SkillInventory.cs
+++ Bearlike Project/Assets/01.Scripts/12.Skill/SkillInventory.cs
@@ -1,9 +1,44 @@
-﻿using UI.Inventory;
+﻿using Manager;
+using UI.Inventory;
+using Unity.VisualScripting;
 
 namespace Skill
 {
     public class SkillInventory : InventoryBase<SkillBase, SkillUIHandle>
     {
+        // 스킬은 이미 객체가 생성된 상태로 존재하는것을 보내주기에 프리펩을 따로 안만들게 한다.
+        public override void AddItem(SkillBase item)
+        {
+            if (itemHashSet.TryGetValue(item, out var hashItem))
+            {
+                if (hashItem is IInventoryItemAdd inventoryItemAdd)
+                {
+                    inventoryItemAdd.AddItem(item);
+                }
+                var handle = uiHandleDictionary[hashItem];
+                if (handle.TryGetComponent(out IInventoryUIUpdate handleUpdateInterface))
+                {
+                    handleUpdateInterface.UIUpdateFromItem(hashItem);
+                }
+
+                item = hashItem;
+            }
+            else
+            {
+                itemHashSet.Add(item);
+
+                var handle = Instantiate(blockUIPrefab, uiParentTransform).GetComponent<SkillUIHandle>();
+                handle.gameObject.SetActive(true);
+                if (handle.TryGetComponent(out IInventoryUIUpdate handleUpdateInterface))
+                {
+                    handleUpdateInterface.UIUpdateFromItem(item);
+                }
+                
+                uiHandleDictionary.Add(item, handle);
+            }
+
+            DebugManager.Log($"[{name}] Inventory에 {item.name}을 추가");
+        }
     }
 }
 
