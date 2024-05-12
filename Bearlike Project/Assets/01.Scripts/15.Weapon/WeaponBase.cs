690f970d15ac28edfdc0792bca2a975a26c18d68
Go In Ho <58318769+k0301h@users.noreply.github.com>
2024-05-13 02:21:30 +0900

    Muzzle Layer 각 클라에만 반영

--- Bearlike Project/Assets/01.Scripts/15.Weapon/WeaponBase.cs
+++ Bearlike Project/Assets/01.Scripts/15.Weapon/WeaponBase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Status;
 using Fusion;
+using Manager;
 using Player;
 using UnityEngine;
 using UnityEngine.VFX;
@@ -72,6 +73,12 @@ namespace Weapon
                 {
                     r.gameObject.layer = LayerMask.NameToLayer("Weapon");
                 }
+
+                DebugManager.ToDo("Muzzle Layer 설정 변경해야함");
+                var muzzle_transform = transform.Find("Muzzle");
+                
+                if(null != muzzle_transform)
+                    muzzle_transform.gameObject.layer = LayerMask.NameToLayer("Weapon");
             }
         }
         
