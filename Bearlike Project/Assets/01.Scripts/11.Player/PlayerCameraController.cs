61ceaac5c9bd25c7071026f8dae04cec5cdd7353
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-05-14 20:10:51 +0900

    카메라 렌즈 외곡 추가

--- Bearlike Project/Assets/01.Scripts/11.Player/PlayerCameraController.cs
+++ Bearlike Project/Assets/01.Scripts/11.Player/PlayerCameraController.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Linq;
 using DG.Tweening;
 using Fusion;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
-using UnityEngine.Serialization;
+using DebugManager = Manager.DebugManager;
 
 namespace Player
 {
@@ -21,8 +24,16 @@ namespace Player
         [Header("카메라")] 
         public Camera targetCamera;
         public Camera weaponCamera;
+        public Camera uiCamera;
         public Vector3 firstOffset;
         public Vector3 thirdOffset;
+
+        private Volume _globalVolume;
+
+        private void Awake()
+        {
+            _globalVolume = FindObjectsOfType<Volume>().First(v => v.isGlobal);
+        }
 
         public override void Spawned()
         {
@@ -100,6 +111,82 @@ namespace Player
         {
             StartCoroutine(ReboundCameraBackCoroutine());
         }
+
+        #region Volume Function
+
+        public void SetActiveLensDistortion(bool value) 
+        {
+            if (_globalVolume.profile.TryGet(out LensDistortion lensDistortion))
+                lensDistortion.active = value;
+            else
+                DebugManager.LogWarning("Lens Distortion이 없습니다.");
+        }
+
+        public void SetLensDistortion(
+            float intensity = 0f, 
+            float multiplierX = 1f,
+            float multiplierY = 1f,
+            Vector2? center = null,
+            float scale = 1f, 
+            float durationTime = 0f)
+        {
+            if (_globalVolume.profile.TryGet(out LensDistortion lensDistortion))
+            {
+                center ??= new Vector2(0.5f, 0.5f);
+
+                if (durationTime == 0f)
+                {
+                    lensDistortion.intensity.value = intensity;
+                    lensDistortion.xMultiplier.value = multiplierX;
+                    lensDistortion.yMultiplier.value = multiplierY;
+                    lensDistortion.center.value = center.Value;
+                    lensDistortion.scale.value = scale;
+                }
+                else
+                {
+                    if(_lensDistortionDurationCoroutine != null) StopCoroutine(_lensDistortionDurationCoroutine); 
+                    _lensDistortionDurationCoroutine = StartCoroutine(LensDistortionDurationCoroutine(lensDistortion, intensity, multiplierX, multiplierY, center.Value, scale, durationTime));
+                }
+            }
+            else
+                DebugManager.LogWarning("Lens Distortion이 없습니다.");
+        }
+
+        private Coroutine _lensDistortionDurationCoroutine;
+        private IEnumerator LensDistortionDurationCoroutine(
+            LensDistortion lensDistortion,
+            float intensity, 
+            float multiplierX,
+            float multiplierY,
+            Vector2 center,
+            float scale, 
+            float durationTime)
+        {
+            var timer = 0f;
+            var normalizeTime = 0f;
+
+            float originIntensity = lensDistortion.intensity.value;
+            float originXMultiplier = lensDistortion.xMultiplier.value;
+            float originYMultiplier = lensDistortion.yMultiplier.value;
+            Vector2 originCenter = lensDistortion.center.value;
+            float originScale = lensDistortion.scale.value;
+            while (timer < durationTime)
+            {
+                timer += Time.deltaTime;
+                normalizeTime = timer / durationTime;
+
+                lensDistortion.intensity.value = Mathf.Lerp(originIntensity, intensity, normalizeTime);
+                lensDistortion.xMultiplier.value = Mathf.Lerp(originIntensity, originXMultiplier, normalizeTime);
+                lensDistortion.yMultiplier.value = Mathf.Lerp(originIntensity, originYMultiplier, normalizeTime);
+                lensDistortion.center.value = Vector2.Lerp(originCenter, center, normalizeTime);
+                lensDistortion.scale.value = Mathf.Lerp(originScale, scale, normalizeTime);
+                
+                yield return null;
+            }
+        }
+        
+        #endregion
+
 
         private IEnumerator ReboundCameraBackCoroutine()
         {
