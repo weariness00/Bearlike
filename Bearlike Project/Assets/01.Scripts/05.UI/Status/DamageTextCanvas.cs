6a9386877646f07a094f7691416f370e33154428
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-05-20 22:38:14 +0900

    툰 쉐이더, 대미지 텍스트
    툰 쉐이더 적용해보는중
    데미지 텍스트 변경

--- Bearlike Project/Assets/01.Scripts/05.UI/Status/DamageTextCanvas.cs
+++ Bearlike Project/Assets/01.Scripts/05.UI/Status/DamageTextCanvas.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
@@ -7,13 +6,22 @@ using Util;
 
 namespace UI.Status
 {
+    public enum DamageTextType
+    {
+        Normal,
+        Critical,
+        Heal,
+    }
+    
     public class DamageTextCanvas : Singleton<DamageTextCanvas>
     {
         private Canvas _canvas;
-        public TMP_Text damageText;
+        public TMP_Text normalDamageText;
+        public TMP_Text criticalDamageText;
+        public TMP_Text healText;
 
         private Camera _camera;
-        private List<Tuple<Vector3,TMP_Text>> _damageTextList = new List<Tuple<Vector3,TMP_Text>>();
+        private List<DamageTextInfo> _damageTextList = new List<DamageTextInfo>();
 
         protected override void Awake()
         {
@@ -25,27 +33,44 @@ namespace UI.Status
         {
             for (var i = 0; i < _damageTextList.Count; i++)
             {
-                var (originPosition, dt) = _damageTextList[i];
-                if (!dt)
+                DamageTextInfo dtInfo = _damageTextList[i];
+                if (!dtInfo.text)
                 {
                     _damageTextList.RemoveAt(i);
                     continue;
                 }
-                dt.gameObject.SetActive(!IsBehindCamera(_camera, originPosition));
-                if(!dt.gameObject.activeSelf) continue;
                 
-                float dir = Vector3.Distance(originPosition, _camera.transform.position);
+                // 카메라 뒤에 있으면 off
+                dtInfo.text.gameObject.SetActive(!IsBehindCamera(_camera, dtInfo.originPosition));
+                if(!dtInfo.text.gameObject.activeSelf) continue;
+                
+                // 거리에 따라 scale 조정
+                float dir = Vector3.Distance(dtInfo.originPosition, _camera.transform.position);
                 float dtScale = 1f;
                 if (dir > 5f) dtScale = 5f / dir * _canvas.scaleFactor;
 
-                dt.rectTransform.localScale = new Vector3(dtScale, dtScale, dtScale);
-                dt.rectTransform.position = _camera.WorldToScreenPoint(originPosition);
+                // ui상의 위치 업데이트
+                dtInfo.text.rectTransform.localScale = new Vector3(dtScale, dtScale, dtScale) * dtInfo.multipleScale;
+                dtInfo.text.rectTransform.position = _camera.WorldToScreenPoint(dtInfo.originPosition);
+                dtInfo.originPosition += Vector3.up * Time.deltaTime;
             }
         }
 
-        public static void SpawnDamageText(Vector3 position, int damage) => Instance.SpawnDamageText_(position, damage);
+        private TMP_Text GetDamageText(DamageTextType type)
+        {
+            if (type == DamageTextType.Normal)
+                return normalDamageText;
+            if (type == DamageTextType.Critical)
+                return criticalDamageText;
+            if (type == DamageTextType.Heal)
+                return healText;
+
+            return null;
+        }
+
+        public static void SpawnDamageText(Vector3 position, int damage, DamageTextType type = DamageTextType.Normal) => Instance.SpawnDamageText_(position, damage, type);
         
-        private void SpawnDamageText_(Vector3 position, int damage)
+        private void SpawnDamageText_(Vector3 position, int damage, DamageTextType type)
         {
             if (!_camera)
             {
@@ -53,12 +78,18 @@ namespace UI.Status
                 _canvas.worldCamera = _camera;
             }
             
-            var dtObj = Instantiate(damageText.gameObject, transform);
+            // Damage Text 생성
+            var dtObj = Instantiate(GetDamageText(type).gameObject, transform);
             var dt = dtObj.GetComponent<TMP_Text>();
-            dt.text = damage.ToString();
-            dt.DOFade(0, 3f);
+            dtObj.SetActive(true);
+            dt.text = type == DamageTextType.Heal ? "+" + damage : damage.ToString();
+            dt.DOFade(0, 3f); // Alpha 감소
+
+            // dt 생성
+            var dtInfo = new DamageTextInfo() { originPosition = position, text = dt };
+            if (type == DamageTextType.Critical) dtInfo.multipleScale = 1.5f;
             
-            _damageTextList.Add(new Tuple<Vector3, TMP_Text>(position, dt));
+            _damageTextList.Add(dtInfo);
             Destroy(dtObj, 3f);
         }
         
@@ -68,6 +99,13 @@ namespace UI.Status
             float dotProduct = Vector3.Dot(camera.transform.forward, toTarget);  // 카메라 전방 벡터와 대상 방향 벡터의 내적 계산
 
             return dotProduct < 0;  // 내적 결과가 0보다 작으면 true 반환
+        }
+
+        private class DamageTextInfo
+        {
+            public float multipleScale = 1f;
+            public Vector3 originPosition;
+            public TMP_Text text;
         }
     }
 }
---
+++ Bearlike Project/Assets/03.Resources/05.Shader/Shell Shader/LightingCelShaded.cginc
@@ -0,0 +1,104 @@
+﻿#ifndef LIGHTING_CEL_SHADED_INCLUDED
+#define LIGHTING_CEL_SHADED_INCLUDED
+
+// 필요한 HLSL 파일을 포함
+// #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+#ifndef SHADERGRAPH_PREVIEW
+struct EdgeConstants
+{
+    float diffuse;
+    float specular;
+    float specularOffset;
+    float distanceAttenuation;
+    float shadowAttenuation;
+    float rim;
+    float rimOffset;
+};
+
+struct SurfaceVariables
+{
+    float3 normal;
+    float3 view;
+    float smoothness;
+    float shininess;
+    float rimThreshold;
+    EdgeConstants ec;
+};
+
+float3 calculateCelShading(Light l, SurfaceVariables s)
+{
+    float shadowAttenuationSmoothstepped = smoothstep(0.0f, s.ec.shadowAttenuation, l.shadowAttenuation);
+    float distanceAttenuationSmoothstepped = smoothstep(0.0f, s.ec.distanceAttenuation, l.distanceAttenuation);
+    
+    float attenuation = shadowAttenuationSmoothstepped * distanceAttenuationSmoothstepped;
+    float diffuse = saturate(dot(s.normal, l.direction));
+    diffuse *= attenuation;
+    diffuse = diffuse > 0 ? 1 : 0;
+
+    float3 h = SafeNormalize(l.direction + s.view);
+    float specular = saturate(dot(s.normal, h));
+    specular = pow(specular, s.shininess);
+    specular *= diffuse * s.smoothness;
+    specular = specular > 0.2 ? 1 : 0;
+
+    float rim = 1 - dot(s.view ,s.normal);
+    rim *= pow(diffuse, s.rimThreshold);
+    rim = rim > 0.75 ? 1: 0;
+
+    diffuse = smoothstep(0.0f, s.ec.diffuse, diffuse);
+    specular = s.smoothness * smoothstep((1 - s.smoothness) * s.ec.specular + s.ec.specularOffset, s.ec.specular + s.ec.specularOffset, specular);
+    rim = s.smoothness * smoothstep(s.ec.rim - 0.5f * s.ec.rimOffset, s.ec.rim + 0.5f * s.ec.rimOffset, rim);
+    
+    return l.color * (diffuse + max(specular, rim));
+}
+
+#endif
+
+void LightingCelShaded_float(
+    float Smoothness,float RimThreshold,float3 Position,float3 Normal,float3 View,
+    float EdgeDiffuse, float EdgeSpecular, float EdgeSpecularOffset, float EdgeDistanceAttenuation, float EdgeShadowAttenuation, float EdgeRim, float EdgeRimOffset,
+    
+    out float3 Color)
+{
+    #if defined(SHADERGRAPH_PREVIEW)
+    Color = float3(1.0f, 0.0f, 1.0f); // 예제 색상
+    
+    #else
+    SurfaceVariables s;
+    s.view = SafeNormalize(View);
+    s.smoothness = Smoothness;
+    s.normal = normalize(Normal);
+    s.shininess = exp2(10 * Smoothness + 1);
+    s.rimThreshold = RimThreshold;
+    s.ec.diffuse = EdgeDiffuse;
+    s.ec.specular = EdgeSpecular;
+    s.ec.specularOffset = EdgeSpecularOffset;
+    s.ec.distanceAttenuation = EdgeDistanceAttenuation;
+    s.ec.shadowAttenuation = EdgeShadowAttenuation;
+    s.ec.rim = EdgeRim;
+    s.ec.rimOffset = EdgeRimOffset;
+    
+    #if SHADOWS_SCREEN
+    float4 clipPos = TransformWorldToHClip(Position);
+    float4 shadowCoord = ComputeScreenPos(clipPos);
+
+    #else
+    float4 shadowCoord = TransformWorldToShadowCoord(Position);
+    
+    #endif
+    
+    Light light = GetMainLight(shadowCoord);
+    Color = calculateCelShading(light, s);
+
+    const int pixelLightCount = GetAdditionalLightsCount();
+    for (int i =0; i < pixelLightCount; ++i)
+    {
+        light = GetAdditionalLight(i, Position, 1);
+        Color += calculateCelShading(light,s);
+    }
+    
+    #endif
+}
+
+#endif
