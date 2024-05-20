3ee47bf194c06c598bd0b11f25401859b4e2b0c1
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-05-21 00:53:08 +0900

    툰 쉐이더 몇개 적용해봄

--- Bearlike Project/Assets/01.Scripts/13.Item/Container/Experience.cs
+++ Bearlike Project/Assets/01.Scripts/13.Item/Container/Experience.cs
@@ -6,7 +6,21 @@ namespace Item.Container
 {
     public class Experience : ItemBase
     {
+        private int _expAmount = 0;
+        
         #region Unity Event Function
+
+        public override void Awake()
+        {
+            base.Awake();
+
+            var statusData = GetStatusData(Id);
+            int defaultAmount = statusData.GetInt("Experience Default Amount");
+            int minAmount = statusData.GetInt("Experience Min Amount");
+            int maxAmount = statusData.GetInt("Experience Max Amount");
+
+            _expAmount = defaultAmount + Random.Range(minAmount, maxAmount);
+        }
 
         public void Update()
         {
@@ -26,6 +40,23 @@ namespace Item.Container
                 StartCoroutine(MoveTargetCoroutine(other.gameObject));
             }
         }
+
+        #endregion
+
+        #region Member Function
+
+        public override void GetItem(GameObject targetObject)
+        {
+            PlayerController pc;//  
+            if (targetObject.TryGetComponent(out pc) || targetObject.transform.root.TryGetComponent(out pc))
+            {
+                pc.status.experience.Current += _expAmount;
+            }
+        }
+
+        public void SetRandomExp(int min, int max) => _expAmount = Random.Range(min, max);
+        public void SetExp(int value) => _expAmount = value;
+        public int GetExp() => _expAmount;
 
         #endregion
     }
