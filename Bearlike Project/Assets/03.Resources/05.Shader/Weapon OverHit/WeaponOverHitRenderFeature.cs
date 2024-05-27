63a10da984a2405d7b605dabb6286cc967004bd1
Seo Dong Woo <ehddn917@gmail.com>
2024-05-28 00:29:14 +0900

    머지후 변경사항

---
+++ Bearlike Project/Assets/03.Resources/05.Shader/Weapon OverHit/WeaponOverHitRenderFeature.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace ShaderExtension
+{
+    public class WeaponOverHitRenderFeature : ScriptableRendererFeature
+    {
+        class CustomRenderPass : ScriptableRenderPass
+        {
+            private RenderTargetHandle renderTextureHandle;
+            private Material material;
+
+            public CustomRenderPass(Material material)
+            {
+                this.material = material;
+                renderTextureHandle.Init("_RenderTexture");
+            }
+
+            public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+            {
+                cmd.GetTemporaryRT(renderTextureHandle.id, cameraTextureDescriptor);
+                ConfigureTarget(renderTextureHandle.Identifier());
+                ConfigureClear(ClearFlag.All, Color.black);
+            }
+
+            public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+            {
+                CommandBuffer cmd = CommandBufferPool.Get("CustomRenderPass");
+                Blit(cmd, renderingData.cameraData.renderer.cameraColorTarget, renderTextureHandle.Identifier(), material);
+                Blit(cmd, renderTextureHandle.Identifier(), renderingData.cameraData.renderer.cameraColorTarget);
+                context.ExecuteCommandBuffer(cmd);
+                CommandBufferPool.Release(cmd);
+            }
+
+            public override void FrameCleanup(CommandBuffer cmd)
+            {
+                cmd.ReleaseTemporaryRT(renderTextureHandle.id);
+            }
+        }
+
+        public Material material;
+        CustomRenderPass renderPass;
+
+        public override void Create()
+        {
+            renderPass = new CustomRenderPass(material)
+            {
+                renderPassEvent = RenderPassEvent.AfterRenderingOpaques
+            };
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            renderer.EnqueuePass(renderPass);
+        }
+    }
+}
