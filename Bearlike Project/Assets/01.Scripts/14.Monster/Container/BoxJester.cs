b76c6d0022f9138687121278085598af3f70c3b0
inhogo(2019180003) <dlsghdmsqlc@naver.com>
2024-06-09 19:50:22 +0900

    BT : BoxJester 구현 준비 중
    BT 구상 중

---
+++ Bearlike Project/Assets/01.Scripts/14.Monster/Container/BoxJester.cs
@@ -0,0 +1,34 @@
+﻿using BehaviorTree.Base;
+using Sound;
+using UnityEngine.Serialization;
+
+namespace Monster.Container
+{
+    public class BoxJester : MonsterBase
+    {
+        private BehaviorTreeRunner _behaviorTreeRunner;
+
+        public SoundBox soundBox;
+
+        #region Unity Event Function
+
+        void Awake()
+        {
+            
+        }
+
+        #endregion
+
+        #region Member Function
+
+        
+
+        #endregion
+
+        #region BT Function
+
+        
+
+        #endregion
+    }
+}
