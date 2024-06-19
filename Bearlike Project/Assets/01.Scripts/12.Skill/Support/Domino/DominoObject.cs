d37d798ee4fad135b6f5715d2517bff8b09791e2
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-06-19 18:29:05 +0900

    도미노 제작 중

--- Bearlike Project/Assets/01.Scripts/12.Skill/Support/Domino/DominoObject.cs
+++ Bearlike Project/Assets/01.Scripts/12.Skill/Support/Domino/DominoObject.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Fusion;
 using GamePlay;
 using Monster;
@@ -15,9 +17,14 @@ namespace Skill.Support
     {
         [Networked] public NetworkId SkillId { get; set; }
 
+        [SerializeField] private GameObject damageDominoObject;
+        [SerializeField] private GameObject collisionEffectObject;
+
         private readonly HashSet<GameObject> _damageMonsterSet = new HashSet<GameObject>(); // 이미 대미지를 입은 대상은 대미지를 다시 입으면 안됨으로 사용
         
         private StatusBase _status;
+
+        private Vector3 _destinationPosition; // 도미노가 닿을 목적지
 
         private void OnTriggerEnter(Collider other)
         {
@@ -32,6 +39,12 @@ namespace Skill.Support
                     _damageMonsterSet.Add(otherStatus.gameObject);
                 }
             }
+        }
+
+        private void OnDestroy()
+        {
+            if (TryGetComponent(out NetworkTransform nt)) Destroy(nt);
+            if (TryGetComponent(out NetworkObject no)) Destroy(no);
         }
 
         public override void Spawned()
