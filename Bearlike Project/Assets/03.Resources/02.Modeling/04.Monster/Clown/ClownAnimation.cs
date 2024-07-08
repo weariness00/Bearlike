bc8ff70fee5863bb2c097cc44136f351a800649b
Kim Ho Young <ho0mama1@naver.com>
2024-07-08 20:07:12 +0900

    Merge
    living room 일부 추가
    광대 보스 추가
    도미노, 총알 상자, 보물상자 등 여러 잡다한 아이템 추가

---
+++ Bearlike Project/Assets/03.Resources/02.Modeling/04.Monster/Clown/ClownAnimation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClownAnimation : MonoBehaviour
+{
+    public enum AttackBlend
+    {
+        Raser,
+        Boom
+    }
+
+    public enum FaceHideBlend
+    {
+        Hide,
+        ReverseHide
+    }
+}
