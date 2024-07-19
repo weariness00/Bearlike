58a73e41f3d142ea8df6e6aa41b708a85953e7c7
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-07-19 23:52:48 +0900

    Unity Evnet Util 추가
    유니티의 특정 이벤트를 특정 오브젝트가 사용 할 수 있는 컴포넌트를 제작

---
+++ Bearlike Project/Assets/01.Scripts/02.Util/UnityEventComponent/UnityEventUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Util.UnityEventComponent
+{
+    public class UnityEventUtil : MonoBehaviour
+    {
+        public void AddOnTriggerEnter(Action<Collider> triggerEnterAction)
+        {
+            if (gameObject.TryGetComponent(out OnTriggerEnterUtil util) == false)
+                util = gameObject.AddComponent<OnTriggerEnterUtil>();
+            
+            util.onTriggerEnterAction += triggerEnterAction;
+        }
+
+        public void RemoveOnTriggerEnter(Action<Collider> triggerEnterAction)
+        {
+            if (gameObject.TryGetComponent(out OnTriggerEnterUtil util))
+            {
+                util.onTriggerEnterAction -= triggerEnterAction;
+                if(util.onTriggerEnterAction == null) DestroyImmediate(util);
+            }
+        }
+    }
+}
+
