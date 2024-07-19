56e75fc2e25f6df4915e1ff28e2a244333d90824
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-07-19 21:10:25 +0900

    랜덤한 아이템을 드랍하는 아이템 제작

---
+++ Bearlike Project/Assets/01.Scripts/02.Util/ListExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Util
+{
+    public static class ListExtension
+    {
+        public static T RandomValue<T>(this List<T> list) => list.RandomValue(0, list.Count);
+        public static T RandomValue<T>(this List<T> list, int minValue, int maxValue)
+        {
+            if (list == null || list.Count == 0)
+            {
+                Debug.LogWarning("List가 비어있습니다.");
+                return default;
+            }
+            
+            if (minValue < 0) minValue = 0;
+            if (maxValue > list.Count) minValue = list.Count;
+
+            return list[Random.Range(minValue, minValue)];
+        }
+    }
+}
