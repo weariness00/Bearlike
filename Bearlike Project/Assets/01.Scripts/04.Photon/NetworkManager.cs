9aec647b0f6d8f0341f85b3e9f786391be75562b
Seo Down Woo <112839880+weariness00@users.noreply.github.com>
2024-09-23 16:57:15 +0900

    Host Migraiton 제작 중

--- Bearlike Project/Assets/01.Scripts/04.Photon/NetworkManager.cs
+++ Bearlike Project/Assets/01.Scripts/04.Photon/NetworkManager.cs
@@ -37,10 +37,7 @@ namespace Photon
         private NetworkRunner _runner;
         private SessionInfo[] _sessionInfoAll = Array.Empty<SessionInfo>();
 
-        public bool isCursor;
-
         public Action<SessionInfo[]> SessionListUpdateAction;
-
 
         #region Unity Event Function
 
@@ -146,6 +143,11 @@ namespace Photon
             }
             
             await _runner.JoinSessionLobby(SessionLobby.Shared);
+        }
+
+        private void HostMigrationResume(NetworkRunner runner)
+        {
+            
         }
         
         async Task Matching(GameMode mode, string sessionName)
@@ -394,11 +396,19 @@ namespace Photon
 
         public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
         {
+            DebugManager.Log("Host Migration");
+
+            runner.StartGame(new StartGameArgs()
+            {
+                SceneManager = gameObject.GetOrAddComponent<NetworkSceneManagerDefault>(),
+                HostMigrationToken = hostMigrationToken,
+                HostMigrationResume = HostMigrationResume
+            });
         }
 
         public void OnSceneLoadDone(NetworkRunner runner)
         {
-            DebugManager.Log($"씬 Loading 끝");
+            DebugManager.Log($"씬 Loading 끝"); 
             _isLoadDone = true;
         }
 
